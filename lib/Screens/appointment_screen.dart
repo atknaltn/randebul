acf2d2832df1e855d227e6ba59c1a06a68995ef4
hasanmutlu26 <92545938+hasanmutlu26@users.noreply.github.com>
2021-12-06 12:41:42 +0300

    randevu alma ekranı
    randevu alma ekranı

---
+++ lib/Screens/appointment_screen.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+//import 'package:randebul/Screens/home_screen.dart';
+
+class AppointmentScreen extends StatefulWidget {
+  final String isim;
+  final String proffession;
+  final int puan;
+
+  const AppointmentScreen({
+    Key? key,
+    this.isim = '',
+    this.proffession = '',
+    this.puan = 5,
+  }) :super(key: key);
+
+  @override
+  State<AppointmentScreen> createState() => _AppointmentScreenState();
+}
+
+class _AppointmentScreenState extends State<AppointmentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Randevu Al'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              padding: const EdgeInsets.only(right: 15, left: 15),
+              tooltip: 'Back',
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+        body: ListView(
+          children: [
+            Container(
+              height: 225,
+              decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30))),
+              child:
+                Column(
+                  children: [
+                    const SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        const SizedBox(width: 30),
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10, top: 10),
+                          child: Image.asset(
+                            "assets/testProfile.jpg",
+                            height: 150,
+                            width: 150,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(top: 30),
+                                child: Text(
+                                  widget.isim,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 10),
+                                child: Text(
+                                  widget.proffession,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 15),
+                                child: Text(
+                                  'Puan: ${widget.puan}',
+                                  style: const TextStyle(
+                                    color: Colors.yellow,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5,),
+                    const Text('Hizmetler',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),)
+                  ],
+                ),
+
+            ),
+            const ServiceBox(
+              serviceName: 'Ağırlık Antrenmanı',
+              duration: 60,
+              icerik: '60 dakikalık ağırlık antrenmanı ile kaslarınızı güçlendirin.',
+              fiyat: 50,
+              ),
+            const ServiceBox(
+              serviceName: 'Kardiyo Antrenmanı',
+              duration: 60,
+              icerik: '60 dakikalık kardiyo antrenmanı ile yoğun kalori harcayın.',
+              fiyat: 40,
+            ),
+            const ServiceBox(
+              serviceName: 'Online Görüşme',
+              duration: 30,
+              icerik: '30 dakikalık online görüşme ile sorularınıza cevap bulun.',
+              fiyat: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ServiceBox extends StatelessWidget {
+  final String serviceName;
+  final int duration;
+  final String icerik;
+  final int fiyat;
+
+  const ServiceBox({
+    Key? key, this.serviceName = '', this.duration = 0, this.icerik = '', this.fiyat = 0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      height: 200,
+      decoration: const BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Column(
+        children: [
+          const SizedBox(height: 5),
+          Text(
+            serviceName,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 5),
+          MyCard(
+            cardIcon: Icons.alarm,
+            tur: 'Süre: ',
+            cardText: '$duration dk.',),
+          const SizedBox(height: 5),
+          MyCard(
+            cardIcon: Icons.home_repair_service_outlined,
+            tur: 'İçerik: ',
+            cardText: icerik,),
+          const SizedBox(height: 5),
+          MyCard(
+            cardIcon: Icons.attach_money,
+            tur: 'Fiyat: ',
+            cardText: '\$$fiyat',),
+        ],
+      ),
+    );  }
+}
+
+class MyCard extends StatelessWidget {
+  final IconData cardIcon;
+  final String tur;
+  final String cardText;
+
+  const MyCard({Key? key, this.cardIcon = Icons.email, this.tur = '', this.cardText = ''})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: 10),
+            Icon(cardIcon),
+            const SizedBox(width: 10),
+            Text(tur, style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            Flexible(
+              child: Text(
+                cardText,
+              ),
+            ),
+          ],
+
+      ),
+    );
+  }
+}
+
