322d6ee2fe69b9668e5951ec4e07589dc19dd54e
hasanmutlu26 <92545938+hasanmutlu26@users.noreply.github.com>
2021-12-10 17:04:59 +0300

    randevu al ekranı güncelleme
    randevu al ekranı dummy firebase hizmetler koleksiyonuna bağlandı

--- lib/Screens/appointment_screen.dart
+++ lib/Screens/appointment_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 //import 'package:randebul/Screens/home_screen.dart';
 
@@ -11,15 +12,18 @@ class AppointmentScreen extends StatefulWidget {
     this.isim = '',
     this.proffession = '',
     this.puan = 5,
-  }) :super(key: key);
+  }) : super(key: key);
 
   @override
   State<AppointmentScreen> createState() => _AppointmentScreenState();
 }
 
 class _AppointmentScreenState extends State<AppointmentScreen> {
+  final _firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
+    CollectionReference hizmetlerRef = _firestore.collection('hasan-hizmetler');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -45,93 +49,108 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
                   borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(30),
                       bottomRight: Radius.circular(30))),
-              child:
-                Column(
-                  children: [
-                    const SizedBox(height: 10,),
-                    Row(
-                      children: [
-                        const SizedBox(width: 30),
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 10, top: 10),
-                          child: Image.asset(
-                            "assets/testProfile.jpg",
-                            height: 150,
-                            width: 150,
-                          ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(width: 30),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10, top: 10),
+                        child: Image.asset(
+                          "assets/testProfile.jpg",
+                          height: 150,
+                          width: 150,
                         ),
-                        Container(
-                          margin: const EdgeInsets.only(left: 20),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.only(top: 30),
-                                child: Text(
-                                  widget.isim,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(top: 30),
+                              child: Text(
+                                widget.isim,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 10),
-                                child: Text(
-                                  widget.proffession,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                  ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 10),
+                              child: Text(
+                                widget.proffession,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
                                 ),
                               ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 15),
-                                child: Text(
-                                  'Puan: ${widget.puan}',
-                                  style: const TextStyle(
-                                    color: Colors.yellow,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 15),
+                              child: Text(
+                                'Puan: ${widget.puan}',
+                                style: const TextStyle(
+                                  color: Colors.yellow,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 5,),
-                    const Text('Hizmetler',
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    'Hizmetler',
                     style: TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                    ),)
-                  ],
-                ),
-
-            ),
-            const ServiceBox(
-              serviceName: 'Ağırlık Antrenmanı',
-              duration: 60,
-              icerik: '60 dakikalık ağırlık antrenmanı ile kaslarınızı güçlendirin.',
-              fiyat: 50,
+                    ),
+                  )
+                ],
               ),
-            const ServiceBox(
-              serviceName: 'Kardiyo Antrenmanı',
-              duration: 60,
-              icerik: '60 dakikalık kardiyo antrenmanı ile yoğun kalori harcayın.',
-              fiyat: 40,
             ),
-            const ServiceBox(
-              serviceName: 'Online Görüşme',
-              duration: 30,
-              icerik: '30 dakikalık online görüşme ile sorularınıza cevap bulun.',
-              fiyat: 20,
-            ),
+            StreamBuilder<QuerySnapshot>(
+                stream: hizmetlerRef.snapshots(),
+                builder: (BuildContext context, AsyncSnapshot asyncSnapshot) {
+                  if (asyncSnapshot.hasError) {
+                    return const Center(
+                        child: Text('Bir Hata Oluştu. Lütfen Tekrar Deneyin.'));
+                  } else {
+                    if (asyncSnapshot.hasData) {
+                      dynamic hizmetList = asyncSnapshot.data.docs;
+                      return Flexible(
+                        child: ColumnBuilder(
+
+                            itemCount: hizmetList.length,
+                            itemBuilder: (context, index) {
+                              return ServiceBox(
+                                serviceName: '${hizmetList[index].data()['name']}',
+                                duration: '${hizmetList[index].data()['sure']}',
+                                icerik: '${hizmetList[index].data()['icerik']}',
+                                fiyat: '${hizmetList[index].data()['fiyat']}',
+                              );
+                            }),
+                      );
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }
+                }),
           ],
         ),
       ),
@@ -139,14 +158,19 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
   }
 }
 
+// Yeşil kutucuklar
 class ServiceBox extends StatelessWidget {
   final String serviceName;
-  final int duration;
+  final String duration;
   final String icerik;
-  final int fiyat;
+  final String fiyat;
 
   const ServiceBox({
-    Key? key, this.serviceName = '', this.duration = 0, this.icerik = '', this.fiyat = 0,
+    Key? key,
+    this.serviceName = '',
+    this.duration = '',
+    this.icerik = '',
+    this.fiyat = '',
   }) : super(key: key);
 
   @override
@@ -171,53 +195,90 @@ class ServiceBox extends StatelessWidget {
           MyCard(
             cardIcon: Icons.alarm,
             tur: 'Süre: ',
-            cardText: '$duration dk.',),
+            cardText: '$duration dk.',
+          ),
           const SizedBox(height: 5),
           MyCard(
             cardIcon: Icons.home_repair_service_outlined,
             tur: 'İçerik: ',
-            cardText: icerik,),
+            cardText: icerik,
+          ),
           const SizedBox(height: 5),
           MyCard(
             cardIcon: Icons.attach_money,
             tur: 'Fiyat: ',
-            cardText: '\$$fiyat',),
+            cardText: '\$$fiyat',
+          ),
         ],
-      ),
-    );  }
-}
-
-class MyCard extends StatelessWidget {
-  final IconData cardIcon;
-  final String tur;
-  final String cardText;
-
-  const MyCard({Key? key, this.cardIcon = Icons.email, this.tur = '', this.cardText = ''})
-      : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(width: 10),
-            Icon(cardIcon),
-            const SizedBox(width: 10),
-            Text(tur, style: const TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            Flexible(
-              child: Text(
-                cardText,
-              ),
-            ),
-          ],
-
       ),
     );
   }
 }
 
+// Yeşil kutucukların içindeki satır satır yazılar.
+class MyCard extends StatelessWidget {
+  final IconData cardIcon;
+  final String tur;
+  final String cardText;
+
+  const MyCard(
+      {Key? key,
+      this.cardIcon = Icons.email,
+      this.tur = '',
+      this.cardText = ''})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(width: 10),
+          Icon(cardIcon),
+          const SizedBox(width: 10),
+          Text(
+            tur,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Flexible(
+            child: Text(
+              cardText,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Bunu internettem buldum tüm sayfayı birden kaydırabilmek için gerekliydi
+class ColumnBuilder extends StatelessWidget {
+  final IndexedWidgetBuilder itemBuilder;
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final VerticalDirection verticalDirection;
+  final int itemCount;
+
+  const ColumnBuilder({
+    Key? key,
+    required this.itemBuilder,
+    required this.itemCount,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.verticalDirection = VerticalDirection.down,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: List.generate(itemCount,
+              (index) => itemBuilder(context, index)).toList(),
+    );
+  }
+}
