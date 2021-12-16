ec5cd2a0e96db26405d0c738896bf39cfa327f26
atknaltn <71217766+atknaltn@users.noreply.github.com>
2021-12-16 12:37:46 +0300

    service model changed

--- lib/model/service_model.dart
+++ lib/model/service_model.dart
@@ -4,27 +4,33 @@ import 'package:flutter/material.dart';
 
 class Service extends StatelessWidget {
   final String filePath;
+  final String serviceName;
 
   /// Here is your constructor
-  const Service({Key? key, required this.filePath}) : super(key: key);
+  const Service({Key? key, required this.filePath, required this.serviceName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return _buildTitle(filePath);
+    return _buildTitle(filePath, serviceName);
   }
 
-  Widget _buildTitle(String filePath) {
-    return new Align(
-        child: new Container(
-      alignment: Alignment.topRight,
-      child: SizedBox(
-        height: 120,
-        width: 175,
-        child: Image.asset(
-          filePath,
-          fit: BoxFit.fill,
+  Widget _buildTitle(String filePath, String serviceName) {
+    return new Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.topRight,
+          child: SizedBox(
+            height: 120,
+            width: 175,
+            child: Image.asset(
+              filePath,
+              fit: BoxFit.fill,
+            ),
+          ),
         ),
-      ),
-    ));
+        Text(serviceName)
+      ],
+    );
   }
 }
