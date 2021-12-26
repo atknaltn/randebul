6b7d8e72c567925ec3dacaf2eba8f3276d6ae85a
atknaltn <71217766+atknaltn@users.noreply.github.com>
2021-12-26 13:59:38 +0300

    test

--- lib/Screens/upload_service.dart
+++ lib/Screens/upload_service.dart
@@ -1,10 +1,13 @@
+import 'dart:collection';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class UploadService extends StatefulWidget {
   const UploadService({Key? key}) : super(key: key);
@@ -15,57 +18,191 @@ class UploadService extends StatefulWidget {
 
 class _UploadServiceState extends State<UploadService> {
   File? imageFile;
+  var formKey = GlobalKey<FormState>();
+  var serviceName, serviceCategory, servicePrice;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff000725),
-      appBar: AppBar(
-        backgroundColor: Color(0xffff2fc3),
-        title: Text(
-          "Create a Service",
-          style: TextStyle(color: Color(0xffffffff)),
+        backgroundColor: Color(0xff000725),
+        appBar: AppBar(
+          backgroundColor: Color(0xffff2fc3),
+          title: Text(
+            "Create a Service",
+            style: TextStyle(color: Color(0xffffffff)),
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 15)),
-            Container(
-                child: imageFile == null
-                    ? TextButton(
-                        onPressed: () {
-                          _showDialog();
-                        },
-                        child: Icon(
-                          Icons.add_a_photo_sharp,
-                          size: 80,
-                          color: Color(0xffff2fc3),
-                        ))
-                    : Image.file(
-                        imageFile!,
-                        width: 400,
-                        height: 400,
-                      )),
-            ElevatedButton(
-                onPressed: () {
-                  if (imageFile == null) {
-                    Fluttertoast.showToast(
-                        msg: "Please select an image first",
-                        gravity: ToastGravity.CENTER,
-                        toastLength: Toast.LENGTH_LONG);
-                  } else {
-                    uploadImage();
-                  }
-                },
-                child: Icon(
-                  Icons.upload_sharp,
-                  size: 150,
-                ))
-          ],
-        ),
-      ),
-    );
+        body: Form(
+          key: formKey,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(top: 15)),
+                Container(
+                    child: imageFile == null
+                        ? TextButton(
+                            onPressed: () {
+                              _showDialog();
+                            },
+                            child: Icon(
+                              Icons.add_a_photo_sharp,
+                              size: 80,
+                              color: Color(0xffff2fc3),
+                            ))
+                        : Image.file(
+                            imageFile!,
+                            width: 400,
+                            height: 400,
+                          )),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Theme(
+                          data: ThemeData(
+                            hintColor: Colors.blue,
+                          ),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Please write a service name";
+                              } else {
+                                serviceName = value;
+                              }
+                            },
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              labelText: "Service Name",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                            ),
+                          )),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Theme(
+                          data: ThemeData(
+                            hintColor: Colors.blue,
+                          ),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Please write a service category";
+                              } else {
+                                serviceCategory = value;
+                              }
+                            },
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              labelText: "Service Category",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                            ),
+                          )),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Theme(
+                          data: ThemeData(
+                            hintColor: Colors.blue,
+                          ),
+                          child: TextFormField(
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "Please write a service price";
+                              } else {
+                                servicePrice = value;
+                              }
+                            },
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              labelText: "Service Price",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  borderSide: BorderSide(
+                                      color: Color(0xffff2fc3), width: 1)),
+                            ),
+                          )),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (imageFile == null) {
+                        Fluttertoast.showToast(
+                            msg: "Please select an image first",
+                            gravity: ToastGravity.CENTER,
+                            toastLength: Toast.LENGTH_LONG);
+                      } else {
+                        uploadImage();
+                      }
+                    },
+                    child: Icon(
+                      Icons.upload_sharp,
+                      size: 150,
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 
   Future<void> _showDialog() {
@@ -113,10 +250,28 @@ class _UploadServiceState extends State<UploadService> {
   }
 
   Future<void> uploadImage() async {
-    Reference reference = FirebaseStorage.instance.ref().child("images").child(
-        new DateTime.now().millisecondsSinceEpoch.toString() +
-            "." +
-            imageFile!.path);
-    UploadTask uploadTask = reference.putFile(imageFile!);
+    String? imageURL;
+    if (formKey.currentState!.validate()) {
+      Reference reference = FirebaseStorage.instance
+          .ref()
+          .child("images")
+          .child(new DateTime.now().millisecondsSinceEpoch.toString() +
+              "." +
+              imageFile!.path);
+      UploadTask uploadTask = reference.putFile(imageFile!);
+      uploadTask.then((imageurl) {
+        imageURL = imageurl.ref.getDownloadURL().toString();
+      });
+      DatabaseReference databaseReference =
+          FirebaseDatabase.instance.ref().child("Data");
+      String? uploadID = databaseReference.push().key;
+      HashMap map = new HashMap();
+      map["serviceName"] = serviceName;
+      map["serviceCategory"] = serviceCategory;
+      map["servicePrice"] = servicePrice;
+      map["imageURL"] = imageURL;
+
+      databaseReference.child(uploadID!).set(map);
+    }
   }
 }
