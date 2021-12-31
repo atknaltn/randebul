7231a893bfac1b48950d15ccb7a287937e106394
hasanmutlu26 <hmutlu2626@gmail.com>
2021-12-31 13:30:51 +0300

    firebase bağlantılı takvim ve tarih-saat seçim ekranı

--- lib/Screens/confirm_appointment.dart
+++ lib/Screens/confirm_appointment.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:randebul/Screens/appointment_screen.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class ConfirmAppointmentPage extends StatefulWidget {
   final dynamic hocaRef;
@@ -17,6 +19,12 @@ class ConfirmAppointmentPage extends StatefulWidget {
 class _ConfirmAppointmentPageState extends State<ConfirmAppointmentPage> {
   DateTime selectedDate = DateTime.now();
   bool validTime = true;
+
+  String? _subjectText = '',
+      _startTimeText = '',
+      _endTimeText = '',
+      _dateText = '',
+      _timeDetails = '';
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +77,7 @@ class _ConfirmAppointmentPageState extends State<ConfirmAppointmentPage> {
                 )
               : const Center(
                   child: Text(
-                    'Lütfen Geçerli Bir Saat Seçiniz:',
+                    'Lütfen Geçerli Bir Tarih ve Saat Seçiniz:',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
@@ -77,16 +85,6 @@ class _ConfirmAppointmentPageState extends State<ConfirmAppointmentPage> {
                     ),
                   ),
                 ),
-          Center(
-            child: Text(
-              'Seçili Tarih: ${selectedDate.day}.${selectedDate.month}.${selectedDate.year} - ${selectedDate.hour}:${selectedDate.minute}',
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-          ),
           ElevatedButton(
             onPressed: () => _openDatePicker(context),
             child: const Text('Tarih Seçin.'),
@@ -94,6 +92,34 @@ class _ConfirmAppointmentPageState extends State<ConfirmAppointmentPage> {
           ElevatedButton(
             onPressed: () => _openTimePicker(context),
             child: const Text('Saat Seçin.'),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: Text(
+              'Seçili Tarih: ${DateFormat('dd.MM.y - HH:mm').format(selectedDate)}',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 550,
+            child: SfCalendar(
+              appointmentTimeTextFormat: 'hh:mm',
+              view: CalendarView.week,
+              firstDayOfWeek: DateTime.now().weekday,
+              timeSlotViewSettings: const TimeSlotViewSettings(
+                timeInterval: Duration(minutes: 30),
+                timeFormat: 'HH:mm',
+                dayFormat: 'EEE',
+                startHour: 9,
+                endHour: 17,
+              ),
+              dataSource: MeetingDataSource(getAppointments()),
+              onTap: calendarTapped,
+            ),
           ),
         ],
       ),
@@ -105,40 +131,53 @@ class _ConfirmAppointmentPageState extends State<ConfirmAppointmentPage> {
       context: context,
       initialDate: selectedDate,
       firstDate: DateTime.now(),
-      lastDate: DateTime(2023),
+      lastDate: DateTime(DateTime.now().year + 2),
     );
 
     if (date != null) {
       setState(() {
+        validTime = true;
         selectedDate = DateTime(date.year, date.month, date.day,
-            selectedDate.hour, selectedDate.minute);
+            DateTime.now().hour, DateTime.now().minute + 5);
       });
     }
   }
 
   _openTimePicker(BuildContext context) async {
+    dynamic randevuList = <Map>[];
+    randevuList = widget.hocaRef['Randevular'];
+    bool conflict = false;
+
     final TimeOfDay? time = await showTimePicker(
       context: context,
-      initialTime: TimeOfDay(hour: selectedDate.hour, minute: selectedDate.minute),
-      builder: (BuildContext context, Widget? child){
-        return Theme(
-          data: ThemeData(),
-          child: MediaQuery(
-            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-            child: child!,
-          ),
-        );
-      }
+      initialTime:
+          TimeOfDay(hour: selectedDate.hour, minute: selectedDate.minute),
     );
 
     if (time != null) {
-      if (time.hour < DateTime.now().hour || (time.hour == DateTime.now().hour &&
-          time.minute < DateTime.now().minute)) {
+      DateTime fullDate = DateTime(selectedDate.year, selectedDate.month,
+          selectedDate.day, time.hour, time.minute);
+      
+      if (fullDate.isBefore(DateTime.now())) {
         setState(() {
           validTime = false;
+          conflict = true;
         });
       }
-      else {
+
+      for(int i = 0; i < randevuList.length; i++){
+        if(fullDate.isAfter(randevuList[i]['startTime'].toDate()) &&
+          fullDate.isBefore(randevuList[i]['startTime'].toDate().add(Duration(minutes: randevuList[i]['duration'])))
+        ) {
+          conflict = true;
+          setState(() {
+            validTime = false;
+          });
+          break;
+        }
+      }
+
+      if(!conflict) {
         setState(() {
           selectedDate = DateTime(selectedDate.year, selectedDate.month,
               selectedDate.day, time.hour, time.minute);
@@ -146,5 +185,124 @@ class _ConfirmAppointmentPageState extends State<ConfirmAppointmentPage> {
         });
       }
     }
+  }
+
+  List<Appointment> getAppointments() {
+    List<Appointment> meetings = <Appointment>[];
+    dynamic randevuList = <Map>[];
+    randevuList = widget.hocaRef['Randevular'];
+
+    for(int i = 0; i < randevuList.length; i++){
+      meetings.add(Appointment(
+          startTime: randevuList[i]['startTime'].toDate(),
+          endTime: randevuList[i]['startTime'].toDate().add(Duration(minutes: randevuList[i]['duration'])),
+          subject: 'Dolu',
+          color: Colors.red,
+          isAllDay: false));
+    }
+    meetings.add(Appointment(
+        startTime: selectedDate,
+        endTime: selectedDate.add(Duration(minutes: widget.selectedHizmet['sure'])),
+        subject: 'Seçilen Tarih',
+        color: Colors.blue,
+        isAllDay: false));
+
+
+    return meetings;
+  }
+
+  void calendarTapped(CalendarTapDetails details) {
+    if (details.targetElement == CalendarElement.appointment ||
+        details.targetElement == CalendarElement.agenda) {
+      final Appointment appointmentDetails = details.appointments![0];
+      _subjectText = appointmentDetails.subject;
+      _dateText = DateFormat('MMMM dd, yyyy')
+          .format(appointmentDetails.startTime)
+          .toString();
+      _startTimeText =
+          DateFormat('HH:mm').format(appointmentDetails.startTime).toString();
+      _endTimeText =
+          DateFormat('HH:mm').format(appointmentDetails.endTime).toString();
+      if (appointmentDetails.isAllDay) {
+        _timeDetails = 'All day';
+      } else {
+        _timeDetails = '$_startTimeText - $_endTimeText';
+      }
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('$_subjectText'),
+              content: SizedBox(
+                height: 80,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          '$_dateText',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: const <Widget>[
+                        Text(''),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(_timeDetails!,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w400, fontSize: 15)),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('close'))
+              ],
+            );
+          });
+    }
+  }
+
+}
+
+List<Appointment> getAppointments(dynamic hocaRef, DateTime selectedDate, Map selectedHizmet) {
+  List<Appointment> meetings = <Appointment>[];
+  dynamic randevuList = <Map>[];
+  randevuList = hocaRef['Randevular'];
+
+  for(int i = 0; i < randevuList.length; i++){
+    meetings.add(Appointment(
+        startTime: randevuList[i]['startTime'].toDate(),
+        endTime: randevuList[i]['startTime'].toDate().add(Duration(minutes: randevuList[i]['duration'])),
+        subject: 'Dolu',
+        color: Colors.red,
+        isAllDay: false));
+  }
+  meetings.add(Appointment(
+      startTime: selectedDate,
+      endTime: selectedDate.add(Duration(minutes: selectedHizmet['sure'])),
+      subject: 'Seçilen Tarih',
+      color: Colors.blue,
+      isAllDay: false));
+
+
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
